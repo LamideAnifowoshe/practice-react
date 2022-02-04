a71beff1e3815d5d6ede04bc5a2c1dc4fbb9253a
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-04 21:52:38 +0100

    this.setState method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,9 +6,10 @@ class Counter extends Component {
     tags: [],
   };
 
-  handleIncrement () {
-      console.log('Increment Clicked')
-  }
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
+  };
 
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
@@ -26,7 +27,12 @@ class Counter extends Component {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={} className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {this.state.tags.length === 0 && "please create a new tag!"}
         {this.renderTags()}
       </div>
@@ -34,7 +40,7 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary" ;
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 
