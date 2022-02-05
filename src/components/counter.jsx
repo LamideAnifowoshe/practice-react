31493dbef4dda0e8a4b5b16f1c76bb9f5fbe5af3
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-02-05 03:46:03 +0100

    compose components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,25 +3,13 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
   };
 
+  //
   handleIncrement = () => {
     console.log("Increment Clicked", this);
     this.setState({ count: this.state.count + 1 });
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   render() {
     return (
@@ -33,8 +21,6 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {this.state.tags.length === 0 && "please create a new tag!"}
-        {this.renderTags()}
       </div>
     );
   }
